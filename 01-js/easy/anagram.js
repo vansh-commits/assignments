4f1f1c8559332e416d5f4d4e810800bcbeac1d5f
vansh-commits <vansh.malhotra439@hotmail.com>
2025-02-08 23:19:23 +0530

    solved 1 question

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,6 +5,20 @@
 */
 
 function isAnagram(str1, str2) {
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
+  if(str1.length != str2.length){
+    return false
+  }
+
+  let ans = true;
+
+  for(let i = 0 ; i<str1.length; i++){
+    if(!str2.includes(str1[i])){
+      ans = false
+    }
+  }
+  return ans;
 
 }
 
